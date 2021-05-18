998c23bbcb05e8fa457d72dd7573cfb055de2cbd
MagcDEV <manuel117g@gmail.com>
2021-05-18 13:23:26 -0430

    layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className='grid grid-rows-layout h-screen text-lg'>
+      <header className='bg-gray-600 text-white flex items-center p-2'>
+        <a className='hover:text-yellow-700' href='/'>
+          React Shopping Cart
         </a>
       </header>
+      <main className=''>Product List</main>
+      <footer className='bg-gray-600 h-20 text-white flex justify-center items-center'>
+        All right is reserved
+      </footer>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@ module.exports = {
   purge: ['./src/**/*.{jx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        layout: '5rem minmax(0, 1fr) 5rem',
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
